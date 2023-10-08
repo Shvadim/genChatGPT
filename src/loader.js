762f9511db2a6723d0e5bbadbb7ed30812761bf1
Shvadim <shvadst@gmail.com>
2023-10-09 01:04:35 +0300

    new file:   .gitignore 	new file:   config/default.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/chatgpt.js 	modified:   src/loader.js 	modified:   src/main.js 	modified:   src/notion.js

--- src/loader.js
+++ src/loader.js
@@ -1,37 +1,30 @@
-import { message } from "telegraf/filters"
+import { message } from "telegraf/filters";
 
 export class Loader {
-    icons = [
-        '🕐','🕑','🕒',
-        '🕓','🕔','🕕',
-        '🕖','🕗','🕘',
-        '🕙','🕚','🕛',
-    ]
-    interval = null
-    message = null
+  icons = ["🕐", "", "🕒", "🕓", "", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚", "🕛"];
+  interval = null;
+  message = null;
 
-    constructor(ctx) {
-        this.ctx = ctx
-    }
+  constructor(ctx) {
+    this.ctx = ctx;
+  }
 
-    async show() {
-        let index = 0
-        this.message = await this.ctx.reply(this.icons[index])
-        this.interval = setInterval(() => {
-            index = index < this.icons.length - 1 ? index+1 : 0
-            this.ctx.telegram.editMessageText(
-                this.ctx.chat.id,
-                this.message.message_id,
-                null,
-                this.icons[index]
-            )
-        }, 500)
-    }
+  async show() {
+    let index = 0;
+    this.message = await this.ctx.reply(this.icons[index]);
+    this.interval = setInterval(() => {
+      index = index < this.icons.length - 1 ? index + 1 : 0;
+      this.ctx.telegram.editMessageText(
+        this.ctx.chat.id,
+        this.message.message_id,
+        null,
+        this.icons[index]
+      );
+    }, 500);
+  }
 
-    hide() {
-        this.ctx.telegram.deleteMessage(this.ctx.chat.id, this.message.message_id)
-        clearInterval(this.interval)
-    }
+  hide() {
+    this.ctx.telegram.deleteMessage(this.ctx.chat.id, this.message.message_id);
+    clearInterval(this.interval);
+  }
 }
-
-
--- src/main.js
+++ src/main.js
@@ -1,33 +1,38 @@
-import { Telegraf } from 'telegraf';
-import { message } from 'telegraf/filters';
-import config from 'config';
-import { Loader } from './loader';
-import { chatGPT } from './chatgpt';
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import config from "config";
+import { chatGPT } from "./chatgpt.js";
+import { Loader } from "./loader.js";
 
-const bot = new Telegraf(config.get('TELEGRAM_TOKEN'), { handlerTimeout: Infinity})
+const bot = new Telegraf(config.get("TELEGRAM_TOKEN"), {
+  handlerTimeout: Infinity,
+});
 
-bot.command('start', (ctx) => ctx.reply('Добро пожаловать. Отправьте текстовое сообщение с тезисами про историю'))
+bot.command("start", (ctx) =>
+  ctx.reply(
+    "Добро пожаловать. Отправьте текстовое сообщение с тезисами про историю"
+  )
+);
 
-bot.on(message('text'), ctx => processGPTResponse)
+bot.on(message("text"), (ctx) => processGPTResponse);
 
 async function processGPTResponse(ctx) {
-    try {
-        const text = ctc.message.text
-        if(!text.trim()) ctx.reply('Текст не может быть пустым!')
+  try {
+    const text = ctx.message.text;
+    if (!text.trim()) ctx.reply("Текст не может быть пустым!");
 
-        const Loader = new Loader(ctx)
-        loader.show()
-        const response = await chatGPT(text)
+    const loader = new Loader(ctx);
+    loader.show();
+    const response = await chatGPT(text);
 
-        if(!response) return ctx.reply(`Ошибка с API. ${response}`)
+    if (!response) return ctx.reply(`Ошибка с API. ${response}`);
 
-        const notionResp = await create(text, response.content)
-        loader.hide()
-        ctx.reply(`Ваша страница: ${notionResp.url}`)
-    } catch (e) {
-        console.log('Error while processing gpt response', e.message)
-    }
+    const notionResp = await create(text, response.content);
+    loader.hide();
+    ctx.reply(`Ваша страница: ${notionResp.url}`);
+  } catch (e) {
+    console.log("Error while processing gpt response", e.message);
+  }
 }
 
-bot.launch()
-
+bot.launch();
